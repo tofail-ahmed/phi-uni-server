ba896fd3ba74542c75efb695cec8a8dec391ac41
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-17 19:08:26 +0600

    cloudinary config added

--- src/app/utils/sendImageToCloudinary.ts
+++ src/app/utils/sendImageToCloudinary.ts
@@ -37,7 +37,7 @@ export const sendImageToCloudinary = (
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, process.cwd() + '/uploads/');
+    cb(null, process.cwd() + '//');
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
